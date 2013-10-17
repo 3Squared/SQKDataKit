0765ff751981b362f06ae5a527510a8ac25b8a70
Sam Oakley <sam.oakley@3squared.com>
2013-10-17 17:02:16 +0100

    Removed customisation block, added initialisers.

--- SQKContextManager.h
+++ SQKContextManager.h
@@ -1,21 +1,18 @@
 // Provides an NSManagedObjectContext on the main thread, and generates a new private NSManagedObjectContext as needed.
 @interface SQKContextManager : NSObject
 
-// Contrcut with a string constant (such as NSSQLiteStoreType) that specifies the store type—see “Store Types” for possible values.
+// Initialise with a string constant (such as NSSQLiteStoreType) that specifies the store type—see “Store Types” for possible values.
 - (instancetype)initWithStoreType:(NSString *)storeType;
-
-// Block used to customise the NSManagedObjectModel instance when it is first created.
-typedef void (^SQKManagedObjectModelCustomisationBlock)(NSManagedObjectModel *managedObjectModel);
-@property (nonatomic, copy) SQKManagedObjectModelCustomisationBlock *managedObjectModelCustomisationBlock;
+- (instancetype)initWithStoreType:(NSString *)storeType managedObjectModel:(NSManagedObjectModel *)managedObjectModel 
 
 // Saves the changes.
 // Changes from any private contexts are merged into the main context at this point
-- (BOOL)saveWithError:(NSError *)error;
+- (BOOL)save:(NSError **)error;
 
 // For large data imports use a private context and SQKJSONDataImportOperation
 - (NSManagedObjectContext *)newPrivateContext;
 
-// For everthing else theres master card / main context
+// For everything else there's master card / main context
 - (NSManagedObjectContext *)mainContext;
 
 @end
