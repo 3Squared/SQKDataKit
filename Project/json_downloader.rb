364d0fc10e2c75fe124237fc54498e60678271ed
Luke Stringer <luke.stringer@3squared.com>
2013-12-23 14:30:24 +0000

    Tweaked ruby JSON downloader script.
    Former-commit-id: e42cda4ccb8943b630514b12d28af66c16ccf74c

--- Project/json_downloader.rb
+++ Project/json_downloader.rb
@@ -7,18 +7,22 @@ all_commits = []
 url = "https://api.github.com/repos/torvalds/linux/commits"
 token = "62ace8411c371516ebb3f237cfec017312a2bc2a"
 
-500.times do |n|
+iterations = 70
+
+iterations.times do |n|
 	puts n+1
 	open(url, "Authorization" => "token #{token}") do |f| 
+		# Next API endpoint specified in the 'next' header field
 		link_str = f.meta['link']
 		url = link_str.match(/<(.*)>; rel=\"next\", <(.*)>; rel=\"first\"/i).captures[0]
-		# pp link_value
 		commits = JSON.parse(f.read)
 		all_commits = all_commits + commits
 
 	end
 end
 
-File.open("data.json","w") do |f|
+entity_count = all_commits.count
+
+File.open("data_#{entity_count}.json","w") do |f|
   f.write(all_commits.to_json)
 end
