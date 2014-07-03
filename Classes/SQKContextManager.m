9f8c6ef85428ae896dc07b97589d60cc70aab683
Luke Stringer <luke.stringer@3squared.com>
2014-07-03 11:50:10 +0100

    Removed automatically merge context changes.
    This was causing problem in multi-threaded code where it could not be
    guaranteed that a merge was completed before a new private context
    was created from the main context.

--- Classes/SQKContextManager.m
+++ Classes/SQKContextManager.m
@@ -33,7 +33,6 @@
         self.storeType = storeType;
         self.managedObjectModel = managedObjectModel;
         self.persistentStoreCoordinator = [NSPersistentStoreCoordinator sqk_storeCoordinatorWithStoreType:storeType managedObjectModel:managedObjectModel];
-        [self observeForSavedNotification];
     }
     return self;
 }
@@ -44,45 +43,6 @@
         validStoreTypes = @[NSSQLiteStoreType, NSInMemoryStoreType, NSBinaryStoreType];
     }
     return validStoreTypes;
-}
-
-- (void)observeForSavedNotification {
-    [[NSNotificationCenter defaultCenter] addObserver:self
-											 selector:@selector(contextSaveNotificationReceived:)
-												 name:NSManagedObjectContextDidSaveNotification
-											   object:nil];
-}
-
-- (void)contextSaveNotificationReceived:(NSNotification *)notification {
-    /**
-     *  Ensure mainContext is accessed on the main thread.
-     */
-    [_mainContext performBlock:^{
-        NSManagedObjectContext *managedObjectContext = [notification object];
-        if (managedObjectContext.concurrencyType == NSPrivateQueueConcurrencyType) {
-            [managedObjectContext performBlock:^{
-                /**
-                 *  If NSManagedObjectContext from the notitification is a private context
-                 *	then merge the changes into the main context.
-                 */
-                
-                [_mainContext mergeChangesFromContextDidSaveNotification:notification];
-    
-                /**
-                 *  This loop is needed for 'correct' behaviour of NSFetchedResultsControllers.
-                 *
-                 *  NSManagedObjectContext doesn't event fire NSManagedObjectContextObjectsDidChangeNotification for updated objects on merge, only inserted.
-                 *
-                 *  SEE: http://stackoverflow.com/questions/3923826/nsfetchedresultscontroller-with-predicate-ignores-changes-merged-from-different
-                 *  May also have memory implications.
-                 */
-                for (NSManagedObject *object in [[notification userInfo] objectForKey:NSUpdatedObjectsKey]) {
-                    [[_mainContext objectWithID:[object objectID]] willAccessValueForKey:nil];
-                }
-                
-            }];
-        }
-    }];
 }
 
 - (NSManagedObjectContext *)mainContext {
@@ -113,8 +73,5 @@
     return NO;
 }
 
-- (void)dealloc {
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:NSManagedObjectContextDidSaveNotification object:nil];
-}
 
 @end
