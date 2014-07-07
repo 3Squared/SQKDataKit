5006583867b006cbbab53c462a1b228b7e2c0f71
Luke Stringer <luke.stringer@3squared.com>
2014-07-07 12:13:30 +0100

    Updated Apple Doc.

--- Classes/shared/SQKCoreDataOperation.h
+++ Classes/shared/SQKCoreDataOperation.h
@@ -14,6 +14,64 @@
  *  Use an SQKCoreDataOperation when you need to perform work with Core Data off the main thread. You need to subclass and must override the `performWorkPrivateContext:` method, which is where you should perform your work with Core Data. The operation will use it's `SQKContextManager` to obtain a private managed object context. This is passed to the `performWorkPrivateContext:` method for you to use. When your work is complete call the `completeOperationBySavingContext:` method passing in the private context you have used. This saves the (private) managed object context, merges the changes into main context, and finishes operation.
 *   Add the operation to an NSOperationQueue that is not the `mainQueue` so that the computation is performed in a background thread. As a private context is used any insertions, updates, deletions etc must be done in a background thread, and using the correct operation queue will ensure that.
  *  Inspired by the obj-c.io article on threading and concurrency. http://www.objc.io/issue-2/concurrency-apis-and-pitfalls.html
+ *  @discussion Subclass example:
+ *
+```
+#import "AnimalImportOperation.h"
+#import "Animal.h"
+#import "NSManagedObject+SQKAdditions.h"
+
+@interface AnimalImportOperation ()
+@end
+
+@implementation AnimalImportOperation
+
+- (void)performWorkPrivateContext:(NSManagedObjectContext *)context {
+    id animalJSON = [self animalJSONFromWebservice];
+    
+    [Animal SQK_insertOrUpdate:animalJSON
+                uniqueModelKey:@"animalID"
+               uniqueRemoteKey:@"IDAnimal"
+           propertySetterBlock:^(NSDictionary *dictionary, id managedObject) {
+               Animal *animal = (Animal *)managedObject;
+               animal.name = dictionary[@"Name"];
+               animal.age = dictionary[@"Age"];
+           }
+                privateContext:self.privateContext
+                         error:NULL];
+    [context save:NULL];
+}
+
+- (id)animalJSONFromWebservice {
+    NSURL *URL = [NSURL URLWithString:@"http://webservice.com/v1/animal"];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:URL];
+    [request setHTTPMethod:@"GET"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    NSData *reponseData = [NSURLConnection sendSynchronousRequest:request returningResponse:NULL error:NULL];
+    id JSON = reponseData != nil ? [NSJSONSerialization JSONObjectWithData:reponseData options:0 error:NULL] : nil;
+    
+    return JSON
+}
+
+@end
+```
+ *  Using with an operation queue.
+```
+NSOperationQueue *operationQueue = [[NSOperationQueue alloc] init]; // background thread queue
+
+AnimalImportOperation *importOperation = [[AnimalImportOperation alloc] initWithContextManager:self.contextManager];
+
+[importOperation setCompletionBlock:^{
+    // Completion logic here
+	[[NSOperationQueue mainQueue] addOperationWithBlock:^{
+        // You may want to perform this on the main thread
+	}];
+}];
+
+[self.operationQueue addOperation:importOperation];
+```
  */
 @interface SQKCoreDataOperation : NSOperation
 
