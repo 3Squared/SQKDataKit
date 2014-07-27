1bf6494e1dc0f5170aba6eab7a9777a74fbd8c84
Luke Stringer <lukestringer630@gmail.com>
2014-07-27 17:59:38 +0100

    Generated model classes.

---
+++ Examples/SQKCoreDataOperation Example/SQKCoreDataOperation Example/User.h
@@ -0,0 +1,31 @@
+//
+//  User.h
+//  SQKCoreDataOperation Example
+//
+//  Created by Luke Stringer on 27/07/2014.
+//  Copyright (c) 2014 3Squared Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class Commit;
+
+@interface User : NSManagedObject
+
+@property (nonatomic, retain) NSString * username;
+@property (nonatomic, retain) NSNumber * followers;
+@property (nonatomic, retain) NSString * avatarURL;
+@property (nonatomic, retain) NSNumber * following;
+@property (nonatomic, retain) NSString * email;
+@property (nonatomic, retain) NSSet *commits;
+@end
+
+@interface User (CoreDataGeneratedAccessors)
+
+- (void)addCommitsObject:(Commit *)value;
+- (void)removeCommitsObject:(Commit *)value;
+- (void)addCommits:(NSSet *)values;
+- (void)removeCommits:(NSSet *)values;
+
+@end
