3380470b108826758ab1c3db8b67f2f9fe5031aa
Luke Stringer <lukestringer630@gmail.com>
2014-07-27 14:44:06 +0100

    Class for interfacing wit Github API.

---
+++ Examples/SQKCoreDataOperation Example/SQKCoreDataOperation Example/CDOGithubAPIClient.h
@@ -0,0 +1,13 @@
+//
+//  CDOGithubAPIClient.h
+//  SQKCoreDataOperation Example
+//
+//  Created by Luke Stringer on 27/07/2014.
+//  Copyright (c) 2014 3Squared Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CDOGithubAPIClient : NSObject
+
+@end
