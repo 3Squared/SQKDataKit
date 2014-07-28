d3a6705b141b3fefe0014ed1fe91005633251b68
Luke Stringer <luke.stringer@3squared.com>
2014-07-28 16:12:42 +0100

    Operation to obtain Users from Github API and import into core data.

---
+++ Examples/SQKCoreDataOperation Example/SQKCoreDataOperation Example/CDOUserImportOperation.m
@@ -0,0 +1,40 @@
+//
+//  CDOUserImportOperation.m
+//  SQKCoreDataOperation Example
+//
+//  Created by Luke Stringer on 28/07/2014.
+//  Copyright (c) 2014 3Squared Ltd. All rights reserved.
+//
+
+#import "CDOUserImportOperation.h"
+#import "CDOGithubAPIClient.h"
+#import "CDOUserImporter.h"
+#import "NSManagedObject+SQKAdditions.h"
+#import "User.h"
+
+@interface CDOUserImportOperation ()
+@end
+
+@implementation CDOUserImportOperation
+
+- (void)performWorkPrivateContext:(NSManagedObjectContext *)context {
+	NSMutableArray *usersJSON = [NSMutableArray array];
+
+	NSFetchRequest *fetchRequest = [User sqk_fetchRequest];
+	NSArray *users = [context executeFetchRequest:fetchRequest error:NULL];
+
+	if (users.count > 0) {
+		for (User *user in users) {
+			id JSON = [[CDOGithubAPIClient sharedInstance] getUser:user.username error:NULL];
+			[usersJSON addObject:JSON];
+		}
+		CDOUserImporter *importer = [[CDOUserImporter alloc] initWithManagedObjectContext:context];
+		[importer importJSON:usersJSON];
+		[self completeOperationBySavingContext:context];
+	}
+	else {
+		[self completeOperationBySavingContext:context];
+	}
+}
+
+@end
