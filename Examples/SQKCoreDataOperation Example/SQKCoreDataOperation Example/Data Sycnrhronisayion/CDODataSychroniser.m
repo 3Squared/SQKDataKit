fc9ab7c1f8730737dcbcca4147864de233cecd87
Luke Stringer <luke.stringer@3squared.com>
2014-07-29 10:17:53 +0100

    Reorganised Xcode project and directory.

--- Examples/SQKCoreDataOperation Example/SQKCoreDataOperation Example/Data Sycnrhronisayion/CDODataSychroniser.m
+++ Examples/SQKCoreDataOperation Example/SQKCoreDataOperation Example/Data Sycnrhronisayion/CDODataSychroniser.m
@@ -129,7 +129,7 @@
 	        [self exectuteNextSynchroniseBlock];
 		}
 	    else {
-	        NSLog(@"Users Import Error");
+	        NSLog(@"User Import Error");
 		}
 	}];
 	[self.operationQueue addOperation:operation];
