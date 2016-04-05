fd0322ae5af344276f27baf65ffe00e4db5ab871
Zack Brown <Zack.Brown@3squared.com>
2016-04-05 11:57:28 +0100

    Remove initWithPersistentStoreCoordinator initialiser
    Refactor initialisation to ensure that storeType, managedObjectModel,
    modelNames and storeURL are persisted.

--- Classes/shared/ContextManager/SQKContextManager.m
+++ Classes/shared/ContextManager/SQKContextManager.m
@@ -11,43 +11,48 @@
 #import "NSManagedObjectContext+SQKAdditions.h"
 
 @interface SQKContextManager ()
+
 @property (nonatomic, strong, readwrite) NSString *storeType;
 @property (nonatomic, strong, readwrite) NSManagedObjectModel *managedObjectModel;
-@property (nonatomic, strong, readwrite) NSManagedObjectContext *mainContext;
+@property (nonatomic, strong, readwrite) NSArray *modelNames;
+@property (nonatomic, strong, readwrite) NSURL *storeURL;
+
 @property (nonatomic, strong, readwrite) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (nonatomic, strong, readwrite) NSManagedObjectContext *mainContext;
+
 @end
 
 @implementation SQKContextManager
 
 - (instancetype)initWithStoreType:(NSString *)storeType
-                managedObjectModel:(NSManagedObjectModel *)managedObjectModel
-    orderedManagedObjectModelNames:(NSArray *)modelNames
-                          storeURL:(NSURL *)storeURL
+               managedObjectModel:(NSManagedObjectModel *)managedObjectModel
+   orderedManagedObjectModelNames:(NSArray *)modelNames
+                         storeURL:(NSURL *)storeURL
 {
     if (!storeType || !managedObjectModel || ![[SQKContextManager validStoreTypes] containsObject:storeType])
     {
         return nil;
     }
-
-    return [self initWithPersistentStoreCoordinator:[NSPersistentStoreCoordinator sqk_storeCoordinatorWithStoreType:storeType
-                                                                                                 managedObjectModel:managedObjectModel
-                                                                                     orderedManagedObjectModelNames:modelNames
-                                                                                                           storeURL:storeURL]];
-}
-
-- (instancetype)initWithPersistentStoreCoordinator:(NSPersistentStoreCoordinator *)persistentStoreCoordinator
-{
-    if (!persistentStoreCoordinator)
+    
+    self = [super init];
+    
+    if(self)
     {
-        return nil;
-    }
-    if (self = [super init])
-    {
-        _persistentStoreCoordinator = persistentStoreCoordinator;
+        _persistentStoreCoordinator = [NSPersistentStoreCoordinator sqk_storeCoordinatorWithStoreType:storeType
+                                                                                   managedObjectModel:managedObjectModel
+                                                                       orderedManagedObjectModelNames:modelNames
+                                                                                             storeURL:storeURL];
+        
+        self.storeType = storeType;
+        self.managedObjectModel = managedObjectModel;
+        self.modelNames = modelNames;
+        self.storeURL = storeURL;
+        
         [self observeForSavedNotification];
     }
-
+    
     return self;
+    
 }
 
 + (NSArray *)validStoreTypes
